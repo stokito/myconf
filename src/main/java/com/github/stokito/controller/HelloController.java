8542fa96a6709b44dc11343f709801ae895eb007
fedor1991 <hosten.q@gmail.com>
2015-03-06 17:16:41 +0200

    added JSP

--- src/main/java/com/github/stokito/controller/HelloController.java
+++ src/main/java/com/github/stokito/controller/HelloController.java
@@ -1,14 +1,12 @@
-package com.github.stokito;
+package com.github.stokito.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+@Controller
 public class HelloController {
-
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "hello";
     }
-
 }
