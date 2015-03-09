c925379f860c86c9ff6809ab54510dbf9905e39b
fedor1991 <hosten.q@gmail.com>
2015-03-09 16:46:25 +0200

    HSQLDB

--- src/main/java/com/github/stokito/controller/HelloController.java
+++ src/main/java/com/github/stokito/controller/HelloController.java
@@ -1,12 +1,26 @@
 package com.github.stokito.controller;
 
+import com.github.stokito.domain.Sponsor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
 @Controller
 public class HelloController {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
     @RequestMapping("/")
+    @Transactional
     public String index() {
+        Sponsor sponsor = new Sponsor();
+        sponsor.setName("Sponsor 1");
+        sponsor.setDescription("Sponsor 1");
+        entityManager.persist(sponsor);
         return "hello";
     }
 }
--- src/main/java/com/github/stokito/domain/Sponsor.java
+++ src/main/java/com/github/stokito/domain/Sponsor.java
@@ -16,4 +16,52 @@ public class Sponsor {
     private byte[] logo;
 //    private List<User> users;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public SponsorType getType() {
+        return type;
+    }
+
+    public void setType(SponsorType type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public URL getUrl() {
+        return url;
+    }
+
+    public void setUrl(URL url) {
+        this.url = url;
+    }
+
+    public byte[] getLogo() {
+        return logo;
+    }
+
+    public void setLogo(byte[] logo) {
+        this.logo = logo;
+    }
+
 }
